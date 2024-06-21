bf941c32a06f4769cac80a7f66de825fdb93e6f0
levis <levismunene01@gmail.com>
2024-06-21 13:31:45 +0300

    adjusted backend

--- schemas.py
+++ schemas.py
@@ -1,38 +1,38 @@
-# schemas.py
+# # schemas.py
 
-from pydantic import BaseModel
-from typing import List
-from datetime import date, datetime
+# from pydantic import BaseModel
+# from typing import List
+# from datetime import date, datetime
 
-class HotelBase(BaseModel):
-    name: str
-    picture: str
-    description: str
-    price_per_night: float
-    is_available: bool = True
+# class HotelBase(BaseModel):
+#     name: str
+#     picture: str
+#     description: str
+#     price_per_night: float
+#     is_available: bool = True
 
-class HotelCreate(HotelBase):
-    pass
+# class HotelCreate(HotelBase):
+#     pass
 
-class Hotel(HotelBase):
-    id: int
+# class Hotel(HotelBase):
+#     id: int
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
 
-class BookingBase(BaseModel):
-    hotel_id: int
-    guest_name: str
-    check_in_date: date
-    check_out_date: date
+# class BookingBase(BaseModel):
+#     hotel_id: int
+#     guest_name: str
+#     check_in_date: date
+#     check_out_date: date
 
-class BookingCreate(BookingBase):
-    pass
+# class BookingCreate(BookingBase):
+#     pass
 
-class Booking(BookingBase):
-    id: int
-    booking_date: datetime
-    hotel: Hotel
+# class Booking(BookingBase):
+#     id: int
+#     booking_date: datetime
+#     hotel: Hotel
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
